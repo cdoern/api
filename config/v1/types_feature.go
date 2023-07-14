3ef2a2797f16c34b265ecefa22f83e958c7c1fe6
arkadeepsen <arsen@redhat.com>
2023-07-14 20:26:59 +0530

    Changed EgressFirewallDNSName to DNSNameResolver and moved it to the 'network.openshift.io' group.

--- config/v1/types_feature.go
+++ config/v1/types_feature.go
@@ -180,7 +180,7 @@ var FeatureSets = map[FeatureSet]*FeatureGateEnabledDisabled{
 		with(automatedEtcdBackup).
 		without(machineAPIOperatorDisableMachineHealthCheckController).
 		with(adminNetworkPolicy).
-		with(egressFirewallDNSName).
+		with(DNSNameResolver).
 		toFeatures(defaultFeatures),
 	LatencySensitive: newDefaultFeatures().
 		toFeatures(defaultFeatures),
